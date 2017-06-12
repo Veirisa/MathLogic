44e98e618601ebe538bbfbe302be4a4dc2999def
Аня Родионова <veirisa@Sapphire.local>
2017-06-12 15:01:00 +0300

    files added

--- Homework1/main.cpp
+++ Homework1/main.cpp
@@ -5,18 +5,15 @@ vector<expression*> derivations;
 
 void add_hypotheses(const string& s) {
     size_t i = 0;
-    int balance = 0;
     string new_hypo = "";
     while (s[i] != '|' || s[i + 1] != '-') {
-        if (s[i] == '(') {
-            ++balance;
-        }
-        if (s[i] == ')') {
-            --balance;
+        if (s[i] == '\0' || s[i] == ' ' || s[i] == '\t') {
+            ++i;
+            continue;
         }
         new_hypo += s[i];
         ++i;
-        if (s[i] == ',' && balance == 0) {
+        if (s[i] == ',') {
             ++i;
             hypotheses.push_back(new expression(new_hypo));
             new_hypo = "";
