d0f6330fd30d5f73a7378d70959a04a39222d30d
Аня Родионова <veirisa@Sapphire.local>
2017-06-14 04:42:09 +0300

    files added

--- Homework3/main.cpp
+++ Homework3/main.cpp
@@ -1,6 +1,8 @@
 #include "inequality.h"
 
 int main() {
+    cin.tie();
+    ios_base::sync_with_stdio(0);
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     int a, b;
